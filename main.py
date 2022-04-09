002a74f10f403c34aace74a35ab8d4d52b8a635a
Stepan <nasluzov.stepan@gmail.com>
2022-04-09 14:02:19 +0300

    Autorization form

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/authorization')
+def index():
+    return render_template('authorization_form.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
