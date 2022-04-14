e02442d73665d95929461514185a67a3c35b8e75
Stepan <nasluzov.stepan@gmail.com>
2022-04-14 17:59:58 +0300

    forms update

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from werkzeug.utils import redirect
 from data import db_session
+from forms.user import RegisterForm
+from data.users import *
 
 db_session.global_init("db/baza_danix.db")
 
@@ -8,9 +11,29 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
-@app.route('/authorization')
-def index():
-    return render_template('authorization_form.html')
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
