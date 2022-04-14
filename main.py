b6e13032896dde5aa2babfce6f8dc656b0fa83a0
Stepan <nasluzov.stepan@gmail.com>
2022-04-14 17:38:46 +0300

    some updates

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from data import db_session
+
+db_session.global_init("db/baza_danix.db")
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
